84fe0c7c90ccd21e8609f3da6fdff8d3beeff676
topherCarpediem <topher.carpediem@gmail.com>
2017-10-08 00:06:37 -0700

    .ini file for php

--- home/request_for_data.php
+++ home/request_for_data.php
@@ -5,15 +5,8 @@ if($_SERVER['REQUEST_METHOD'] == 'GET'){
 	if ($_SERVER['HTTP_REFERER'] == 'http://localhost/kms/home/mandates.php') {
 		if($_GET['keyword']){
 			$keyword = $_GET['keyword'];
-			$data = DB::query('SELECT * FROM gad');
-			$matches = array();
-			foreach($data as $k=>$v) {
-			   if(strpos($v[$k], $keyword)){
-			   		print_r('header(string)');
-			   }
-			}
-
-			print_r($matches);
+			$data = DB::query('SELECT * FROM gad where summary LIKE :keyword', array(':keyword'=> '%'. $keyword . '%'));
+			print_r($data);
 		}
 	}
 }
