619edc6350fbd2582826b09902833e10f36ff507
topherCarpediem <topher.carpediem@gmail.com>
2017-10-09 02:04:14 -0700

    about page. Ilagay mo under home directory

---
+++ about.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>KMS</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="../admin/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../admin/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="../admin/bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../admin/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="../admin/dist/css/skins/_all-skins.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-purple layout-top-nav">
+<div class="wrapper">
+
+  <header class="main-header">
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+
+        <div class="navbar-header">
+          <a href="">   <img class="navbar-brand" src="../img/sad.png"></img></a>
+          <a href="index.php" class="navbar-brand"><b>Gender &</b> Development</a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <i class="fa fa-bars"></i>
+          </button>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+          <ul class="nav navbar-nav">
+            
+            <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="mandates.php">GAD Mandates</a></li>
+            <li><a href="linkages.php">Linkages</a></li>
+            <li><a href="ppa.php">PPA</a></li>
+            <li><a href="resources.php">Resources</a></li>
+            <li><a href="connected_sites.php">Connected Sites</a></li>
+            <li><a href="../admin/login.php">Login</a></li>
+           
+          </ul>
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
+            </div>
+          </form>
+        </div>
+      
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+  </header>
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+  
+
+      <!-- Main content -->
+      <section class="content">
+      
+
+       <div class="col">
+         <div class="box box-primary">
+           <div class="box-header">
+             <h1 class="text-center"><b style="color:#605ca8">WHO</b> ARE WE</h1>
+           </div>
+           <div class="box-body">
+                <i><h4 class="text-center">The Gender and Development Agenda (GAD) supports Batangas State University’s vision, mission and strategic goals, as embodied in its Strategic Plan. The GAD Agenda recognizes the various GAD commitments of the University under various gender-related laws and policies, which include, but not limited to, the Philippine Plan for Gender-Responsive Development Plan (1995-2025), <br>Women in Development and Nation Building Act (Republic Act No. 7192), Anti Sexual Harassment Act of 1995 (Republic Act No. 7877), Anti-Violence Against Women and their Children Act of 2004 (Republic Act No. 9262) and the Magna Carta of Women (Republic Act No. 9710), Gender and Development Accord of Philippine Higher Education Institutions, CHED Memorandum Order No. 1, Series of 2015 (Establishing the Policies and Guidelines on Gender and Development in the Commission on Higher Education and Higher Education Institutions).</h4></i><br><br>
+                <hr style="height:1px;border:none;color:#3c8dbc;background-color:#3c8dbc;" />
+                <div class="box-header">
+                   <h1 class="text-center"><b style="color:#605ca8">WHAT</b> WE DO</h1>
+                   <div class="box-body">
+                    <i><h4 class="text-center">
+The primary thrust of Gender and Development is to develop Batangas State University into a leading gender-responsive higher education institution. 
+This will be attained by:<br><br>
+
+Integrating and advocating gender perspectives in the four-fold functions of higher education, i.e., instruction, research, extension and production, and other University operations through gender-responsive policies, programs and mechanisms; and,
+ensuring that every member of the academic community and its stakeholders are gender-responsive through the enhancement of their capacity to use gender lens in the performance of their functions and champion GAD efforts of the University.
+<br><br>
+<b>GOAL 1:</b> To mainstream gender and development in the curriculum, research and extension
+<br><br>
+<b>GOAL 2:</b> To institutionalize gender-responsive mechanisms in the University
+<br><br>
+<b>GOAL 3:</b> To develop and recognize gender responsive members of the academic community and external stakeholders
+<br><br>
+<b>GOAL 4:</b> To establish sustainable partnerships with external stakeholders and technical experts and foster cooperation in the planning, development, implementation, monitoring and evaluation of GAD PAPs
+<br><br>
+<b>GOAL 5:</b> To construct/upgrade facilities which would address gender concerns of stakeholders
+                    </h4></i>
+                   </div>
+               </div>
+
+           </div>
+         </div>
+       </div>
+
+      
+   
+       <!-- /.box -->
+     </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="container" style="text-align: center;">
+      
+      <strong>Copyright & KMS 2017.</strong> All rights
+      reserved.
+    </div>
+    <!-- /.container -->
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="../admin/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../admin/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../admin/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../admin/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../admin/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../admin/dist/js/demo.js"></script>
+</body>
+</html>
